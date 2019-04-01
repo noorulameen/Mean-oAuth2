4871987280fcae70d334cd5a3e874e6dd031dd9e
noorulameen <ameen3388@gmail.com>
2019-04-01 15:38:18 +0530

    Angular manu costom

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -13,7 +13,7 @@ var todoUser = {
     login: function (req, res) {
         User.findOne({username: req.body.username}, function (err, user) {
             if (!user) {
-                res.status(404).send({
+               return res.status(404).send({
                     message: "Wrong username " + req.body.username
                 });
             } else {
